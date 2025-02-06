2879d4959f8f8dbe651e2f7427db3738604f29fb
Ajay Sathe <Ajay Sathe@LAPTOP-SCLIVQOO>
2025-02-06 14:55:27 +0530

    Git By Trupti

---
+++ GitProject1/src/com/cjc/Demo.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("This is demo Class");
+}
+	
+	public void xyx() {
+		System.out.println("This is xyz Method");
+	}
+
+}
